5472693aa332fa69876d150b059662201068bbd0
mia16zang <manya.mankad@gmail.com>
2023-08-25 01:09:51 +0530

    n

--- assign7.js
+++ assign7.js
@@ -5,6 +5,22 @@ function showA() {
 
 const btn2 = document.getElementById("button2");
 const divB = document.getElementById("b");
-btn2.addEventListener("click", function () {
+btn2.addEventListener("click", showB);
+
+function showB() {
   divB.style.display = "block";
+}
+
+const close1 = document.getElementById("close1");
+
+close1.addEventListener("click", () => {
+  const divA = document.getElementById("a");
+  divA.style.display = "none";
+});
+
+const bigger = document.getElementById("bigger");
+
+bigger.addEventListener("click", () => {
+  const widthB = document.getElementById("b").style.width.value + px;
+  widthB += 20;
 });
