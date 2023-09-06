b08940f292b69d8e041c05cc788fe185a971db45
mia16zang <manya.mankad@gmail.com>
2023-09-06 14:11:09 +0530

    n

--- A3.js
+++ A3.js
@@ -1,14 +1,8 @@
-//editable title for header code
-const editableTitle = document.getElementById("editableTitle");
+//editable title for header code saving in localstorage
+document.getElementById("editableTitle").innerHTML =
+  localStorage["editableTitle"] || "Title";
 
-// Retrieve the stored title from local storage (if available)
-const storedTitle = localStorage.getItem("userTitle");
-if (storedTitle) {
-  editableTitle.textContent = storedTitle;
-}
-
-// Listen for input changes and store the updated title in local storage
-editableTitle.addEventListener("input", () => {
-  const newTitle = editableTitle.textContent;
-  localStorage.setItem("userTitle", newTitle);
-});
+setInterval(function () {
+  localStorage["editableTitle"] =
+    document.getElementById("editableTitle").innerHTML;
+}, 20 * 1000);
